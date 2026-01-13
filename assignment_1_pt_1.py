64f1ba9d8e4728b7276c07e8d36c64d3e05dce87
roufbaig <31572668+roufbaig@users.noreply.github.com>
2026-01-13 12:24:39 +0530

    Add files via upload

---
+++ assignment_1_pt_1.py
@@ -0,0 +1,11 @@
+# Task 1: Basic Maths Operations
+n1 = float(input("Enter the first number: "))
+n2 = float(input("Enter the second number: "))
+a = n1 + n2
+s = n1 - n2
+m = n1 * n2
+d = n1 / n2
+print("Addition:",a)
+print("Subtraction:",s)
+print("Multiplication:",m)
+print("Division:",round(d,2))
---
+++ assignment_1_pt_2.py
@@ -0,0 +1,4 @@
+# Task 2: A Personalized Greeting
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello", first_name, last_name, "! Welcome to the Python program.")
